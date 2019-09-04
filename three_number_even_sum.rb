07adb85cf295af787b14211900759ed584b8610b
Rahul <rahul@gammastack.com>
2019-09-04 18:40:31 +0530

    three num even sum program updated succesfuly

--- three_number_even_sum.rb
+++ three_number_even_sum.rb
@@ -1,17 +1,14 @@
 class Integer
-  def solutions?
+  def solutions
     count = 0
-    b=(1..self).to_a.combination(3).to_a
-    (0..b.length-1).each do |i|
-        if b[i].inject(0){|sum, e|  sum + e }.even?
-            count = count+1 
-        end
+    (1..self).to_a.combination(3).each do |comb|
+      count += 1 if comb.inject(0){ |sum, e|  sum + e }.even?
     end
-    return count
+    count
   end
 end
 
 puts "Enter the Range for positive integer no."
 num= gets.to_i
-puts num.solutions?
+puts num.solutions
 
